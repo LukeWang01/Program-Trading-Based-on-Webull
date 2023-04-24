604e4aecdf485c28a5b12adf26204ab4a4120735
Zilu Wang <zwang3208@gatech.edu>
2023-04-24 01:20:55 -0400

    dev 0424
    test yahoo API

--- Quoter_Yahoo.py
+++ Quoter_Yahoo.py
@@ -13,7 +13,7 @@ class Quoter_Yahoo(Quoter):
         self.name = "Yahoo Platform"
 
     """
-        Limit:  a maximum of 10 requests per second per user
+        Limit:  a maximum of 10 requests per second per userw
                 2,000 requests per hour per IP (or up to a total of 48,000 requests a day)
                 
         Response time: around 200 - 400 ms
@@ -24,7 +24,165 @@ class Quoter_Yahoo(Quoter):
             Intraday data cannot extend last 60 days
         interval : str
         return: pandas dataframe
+        
+        https://github.com/ranaroussi/yfinance
+        https://algotrading101.com/learn/yfinance-guide/
     """
-    def get_1min_bar(self, stock, count='max', extend_trading=False):
 
-        data_yf = yf.Ticker(stock).history(period='5d', interval='1m', actions=False, prepost=extend_trading)
+    def get_1min_bar(self, stock, count='max', extend_trading=False):
+        """
+        :param stock: str or list
+        :param count: number of bars
+        :param extend_trading: including before and after trading
+        :return: pandas dataframe or dict of dfs
+        Same to other get_bar functions
+        """
+        if len(stock) == 0:
+            logging_error('stock cannot be empty')
+            return False
+
+        if type(stock) == str or (type(stock) == list and len(stock) == 1):
+            res_df = yf.Ticker(stock).history(period='5d', interval='1m', actions=False, prepost=extend_trading)
+
+            return res_df
+
+        elif type(stock) == list:
+            tickers = yf.Tickers(stock)
+            df_tickers = tickers.history(period='5d', interval='1m', actions=False,
+                                         prepost=extend_trading, group_by="ticker")
+            res_dict = {}
+            for i in stock:
+                res_dict[i] = df_tickers[i]
+
+            return res_dict
+
+    def get_2min_bar(self, stock, count='max', extend_trading=False):
+        if len(stock) == 0:
+            logging_error('stock cannot be empty')
+            return False
+
+        if type(stock) == str or (type(stock) == list and len(stock) == 1):
+            res_df = yf.Ticker(stock).history(interval='2m', actions=False, prepost=extend_trading)
+
+            return res_df
+
+        elif type(stock) == list:
+            tickers = yf.Tickers(stock)
+            df_tickers = tickers.history(interval='2m', actions=False,
+                                         prepost=extend_trading, group_by="ticker")
+            res_dict = {}
+            for i in stock:
+                res_dict[i] = df_tickers[i]
+
+            return res_dict
+
+    def get_5min_bar(self, stock, count='max', extend_trading=False):
+        if len(stock) == 0:
+            logging_error('stock cannot be empty')
+            return False
+
+        if type(stock) == str or (type(stock) == list and len(stock) == 1):
+            res_df = yf.Ticker(stock).history(interval='5m', actions=False, prepost=extend_trading)
+
+            return res_df
+
+        elif type(stock) == list:
+            tickers = yf.Tickers(stock)
+            df_tickers = tickers.history(interval='5m', actions=False,
+                                         prepost=extend_trading, group_by="ticker")
+            res_dict = {}
+            for i in stock:
+                res_dict[i] = df_tickers[i]
+
+            return res_dict
+
+    def get_15min_bar(self, stock, count='max', extend_trading=False):
+        if len(stock) == 0:
+            logging_error('stock cannot be empty')
+            return False
+
+        if type(stock) == str or (type(stock) == list and len(stock) == 1):
+            res_df = yf.Ticker(stock).history(interval='15m', actions=False, prepost=extend_trading)
+
+            return res_df
+
+        elif type(stock) == list:
+            tickers = yf.Tickers(stock)
+            df_tickers = tickers.history(interval='15m', actions=False,
+                                         prepost=extend_trading, group_by="ticker")
+            res_dict = {}
+            for i in stock:
+                res_dict[i] = df_tickers[i]
+
+            return res_dict
+
+    def get_30min_bar(self, stock, count='max', extend_trading=False):
+        if len(stock) == 0:
+            logging_error('stock cannot be empty')
+            return False
+
+        if type(stock) == str or (type(stock) == list and len(stock) == 1):
+            res_df = yf.Ticker(stock).history(interval='30m', actions=False, prepost=extend_trading)
+
+            return res_df
+
+        elif type(stock) == list:
+            tickers = yf.Tickers(stock)
+            df_tickers = tickers.history(interval='30m', actions=False,
+                                         prepost=extend_trading, group_by="ticker")
+            res_dict = {}
+            for i in stock:
+                res_dict[i] = df_tickers[i]
+
+            return res_dict
+
+    def get_1h_bar(self, stock, count='max', extend_trading=False):
+        if len(stock) == 0:
+            logging_error('stock cannot be empty')
+            return False
+        if count == 'max':
+            period = '2y'
+        else:
+            # specific time period current not working
+            pass
+
+        if type(stock) == str or (type(stock) == list and len(stock) == 1):
+            res_df = yf.Ticker(stock).history(period=period, interval='1h', actions=False, prepost=extend_trading)
+
+            return res_df
+
+        elif type(stock) == list:
+            tickers = yf.Tickers(stock)
+            df_tickers = tickers.history(period=period, interval='1h', actions=False,
+                                         prepost=extend_trading, group_by="ticker")
+            res_dict = {}
+            for i in stock:
+                res_dict[i] = df_tickers[i]
+
+            return res_dict
+
+    def get_1d_bar(self, stock, count='max', extend_trading=False):
+        if len(stock) == 0:
+            logging_error('stock cannot be empty')
+            return False
+        if count == 'max':
+            period = '10y'
+        else:
+            # specific time period current not working
+            pass
+
+        if type(stock) == str or (type(stock) == list and len(stock) == 1):
+            res_df = yf.Ticker(stock).history(period=period, interval='1d', actions=False, prepost=extend_trading)
+
+            return res_df
+
+        elif type(stock) == list:
+            tickers = yf.Tickers(stock)
+            df_tickers = tickers.history(period=period, interval='1d', actions=False,
+                                         prepost=extend_trading, group_by="ticker")
+            res_dict = {}
+            for i in stock:
+                res_dict[i] = df_tickers[i]
+
+            return res_dict
+
