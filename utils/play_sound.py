660c36ab9212c21ab784d3ace0f20fad94c63dcc
Zilu Wang <zwang3208@gatech.edu>
2023-05-14 00:10:46 -0400

    bug fix and rename

--- utils/play_sound.py
+++ utils/play_sound.py
@@ -1,15 +1,25 @@
 import simpleaudio as sa
 
+# Note: there will be no sound when the code exit immediately  
+
 
 def order_placed():
     # Load the sound file
-    wave_obj = sa.WaveObject.from_wave_file('../audio/reminders.wav')
+    wave_obj = sa.WaveObject.from_wave_file('../audio/order_placed.wav')
 
     # Play the sound file
     play_obj = wave_obj.play()
 
     # Wait for the sound to finish playing
     # play_obj.wait_done()
-    # print('played')
 
 
+def strategy_notified():
+    # Load the sound file
+    wave_obj = sa.WaveObject.from_wave_file('../audio/strategy_notified.wav')
+
+    # Play the sound file
+    play_obj = wave_obj.play()
+
+    # Wait for the sound to finish playing
+    # play_obj.wait_done()
