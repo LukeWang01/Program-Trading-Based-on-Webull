f19410ff0048f3de341d3b16d855eb7175cbf4a1
Zilu Wang <zwang3208@gatech.edu>
2023-06-12 22:51:16 -0400

    bug fix to switch frames

--- app.py
+++ app.py
@@ -5,47 +5,65 @@ APP GUI Main interface
 LukeLab, 05/2023
 
 """
-
+import ctypes
+import time
 import tkinter as tk
 from pathlib import Path
 
 from tkinter import Tk, Canvas, Entry, PhotoImage
+from tkinter import font as tkfont
 
 from gui.gui_0_DashboardLogin.DashboardLogin import DashboardLogin
+from gui.gui_1_DashboardLogged.DashboardLogged import DashboardLogged
 
 
-# import gui frame classes:
-# from gui.gui_0_DashboardLogin.DashboardLogin import DashboardLogin
-# from gui.gui_1_DashboardLogged.DashboardLogged import DashboardLogged
-# from gui.gui_2_StrategyMonitor.StrategyMonitor import StrategyMonitor
-# from gui.gui_3_TreaderProfile.TreaderProfile import TreaderProfile
-# from gui.gui_4_TradingList.TradingList import TradingList
-# from gui.gui_5_Performance.Performance import Performance
-# from gui.gui_6_APPLog.APPLog import APPLog
-# from gui.gui_7_Message.Message import Message
-# from gui.gui_8_DownloadData.DownloadData import DownloadData
-# from gui.gui_9_SaveExit.SaveExit import SaveExit
-
-
-class TradingApp:
+class TradingApp(tk.Tk):
     def __init__(self, *args, **kwargs):
-        # init app window
-        self.window = tk.Tk()
+        tk.Tk.__init__(self, *args, **kwargs)
 
         # set app icon
-        icon_photo = PhotoImage(file="gui/icon.png")
-        self.window.iconphoto(False, icon_photo)
+        self.icon_photo = PhotoImage(file="gui/icon.png")
+        self.iconphoto(False, self.icon_photo, self.icon_photo)
 
         # set app window size
-        self.window.geometry("1096x728+50+50")
-        self.window.configure(bg="#FFFFFF")
-        self.window.title("Program Trading Based on Webull")
-        self.window.resizable(False, False)
+        self.geometry("1096x728+200+100")
+        self.configure(bg="#FFFFFF")
+        self.title("Program Trading Based on Webull")
+        self.title_font = tkfont.Font(family="Arial Rounded MT Bold", size=18, weight="bold", slant="italic")
+        self.resizable(False, False)
 
-        # set frame 0, DashboardLogin
-        self.frame_0 = DashboardLogin(self)
+        # set frame dict
+        self.frames = {}
+
+        # init frames
+        for F in (DashboardLogin, DashboardLogged):
+            page_name = F.__name__
+            frame = F(self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        # show login frame
+        self.show_frame("DashboardLogin")
+
+        # other settings
+        self.bind("<Button-1>", self.window_clicked)
+        self.cnt = 0
+
+    def window_clicked(self, event):
+        print("clicked")
+        print(event.x, event.y)
+        if event.x < 50 and event.y < 50:
+            self.cnt += 1
+            if self.cnt % 2 == 0:
+                self.show_frame("DashboardLogged")
+            else:
+                self.show_frame("DashboardLogin")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
 if __name__ == "__main__":
     app = TradingApp()
-    app.window.mainloop()
+    app.mainloop()
--- gui/gui_0_DashboardLogin/DashboardLogin.py
+++ gui/gui_0_DashboardLogin/DashboardLogin.py
@@ -1,11 +1,13 @@
-class DashboardLogin:
+import tkinter as tk
+from pathlib import Path
+from tkinter import Canvas, Entry, PhotoImage
 
+
+class DashboardLogin(tk.Frame):
     def __init__(self, parent):
-        import tkinter as tk
-        from pathlib import Path
-
-        from tkinter import Tk, Canvas, Entry, PhotoImage
-        self.frame = tk.Frame(parent.window)
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        # self.controller = controller
 
         def relative_to_assets(path: str) -> Path:
             return ASSETS_PATH / Path(path)
@@ -13,10 +15,8 @@ class DashboardLogin:
         OUTPUT_PATH = Path(__file__).parent
         ASSETS_PATH = OUTPUT_PATH / Path("frame0")
 
-        self.frame.grid(row=0, column=0, sticky="nsew")
-
-        canvas = Canvas(
-            self.frame,
+        self.canvas = Canvas(
+            self,
             bg="#3A59C7",
             height=728,
             width=1096,
@@ -24,75 +24,63 @@ class DashboardLogin:
             highlightthickness=0,
             relief="ridge"
         )
-
-        canvas.place(x=0, y=0)
+        self.canvas.place(x=0, y=0)
 
         # Load images
-        image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
-        image_image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
-        image_image_3 = PhotoImage(file=relative_to_assets("image_3.png"))
-        image_image_4 = PhotoImage(file=relative_to_assets("image_4.png"))
-        entry_image_1 = PhotoImage(file=relative_to_assets("entry_1.png"))
-        entry_image_2 = PhotoImage(file=relative_to_assets("entry_2.png"))
-        entry_image_3 = PhotoImage(file=relative_to_assets("entry_3.png"))
-        image_image_5 = PhotoImage(file=relative_to_assets("image_5.png"))
-        image_image_6 = PhotoImage(file=relative_to_assets("image_6.png"))
-        image_image_7 = PhotoImage(file=relative_to_assets("image_7.png"))
-        image_image_8 = PhotoImage(file=relative_to_assets("image_8.png"))
+        self.image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
+        self.image_image_2 = PhotoImage(file=relative_to_assets("image_2.png"))
+        self.image_image_3 = PhotoImage(file=relative_to_assets("image_3.png"))
+        self.image_image_4 = PhotoImage(file=relative_to_assets("image_4.png"))
+        self.entry_image_1 = PhotoImage(file=relative_to_assets("entry_1.png"))
+        self.entry_image_2 = PhotoImage(file=relative_to_assets("entry_2.png"))
+        self.entry_image_3 = PhotoImage(file=relative_to_assets("entry_3.png"))
+        self.image_image_5 = PhotoImage(file=relative_to_assets("image_5.png"))
+        self.image_image_6 = PhotoImage(file=relative_to_assets("image_6.png"))
+        self.image_image_7 = PhotoImage(file=relative_to_assets("image_7.png"))
+        self.image_image_8 = PhotoImage(file=relative_to_assets("image_8.png"))
 
         # Create canvas objects
-        image_1 = canvas.create_image(548.0, 364.0, image=image_image_1)
-        image_2 = canvas.create_image(645.0, 432.0, image=image_image_2)
-        image_3 = canvas.create_image(1012.0, 30.0, image=image_image_3)
-        image_4 = canvas.create_image(971.0, 30.0, image=image_image_4)
-        entry_bg_1 = canvas.create_image(817.5, 491.5, image=entry_image_1)
-        entry_bg_2 = canvas.create_image(817.5, 397.5, image=entry_image_2)
-        entry_bg_3 = canvas.create_image(817.5, 307.0, image=entry_image_3)
-        image_5 = canvas.create_image(328.0, 30.0, image=image_image_5)
-        image_6 = canvas.create_image(534.0, 30.0, image=image_image_6)
-        image_7 = canvas.create_image(534.0, 30.0, image=image_image_7)
-        image_8 = canvas.create_image(328.0, 30.0, image=image_image_8)
+        self.image_1 = self.canvas.create_image(548.0, 364.0, image=self.image_image_1)
+        self.image_2 = self.canvas.create_image(645.0, 432.0, image=self.image_image_2)
+        self.image_3 = self.canvas.create_image(1012.0, 30.0, image=self.image_image_3)
+        self.image_4 = self.canvas.create_image(971.0, 30.0, image=self.image_image_4)
+        self.entry_bg_1 = self.canvas.create_image(817.5, 491.5, image=self.entry_image_1)
+        self.entry_bg_2 = self.canvas.create_image(817.5, 397.5, image=self.entry_image_2)
+        self.entry_bg_3 = self.canvas.create_image(817.5, 307.0, image=self.entry_image_3)
+        self.image_5 = self.canvas.create_image(328.0, 30.0, image=self.image_image_5)
+        self.image_6 = self.canvas.create_image(534.0, 30.0, image=self.image_image_6)
+        self.image_7 = self.canvas.create_image(534.0, 30.0, image=self.image_image_7)
+        self.image_8 = self.canvas.create_image(328.0, 30.0, image=self.image_image_8)
 
-        entry_1 = Entry(
+        self.entry_1 = Entry(
+            self.canvas,
             bd=0,
             bg="#FFFFFF",
             fg="#000716",
-            highlightthickness=0
+            highlightthickness=0,
+            show="*"
         )
-        entry_1.place(
-            x=688.0,
-            y=474.0,
-            width=259.0,
-            height=33.0
-        )
+        self.entry_1.place(x=688.0, y=474.0, width=259.0, height=33.0)
 
-        entry_2 = Entry(
+        self.entry_2 = Entry(
+            self.canvas,
             bd=0,
             bg="#FFFFFF",
             fg="#000716",
-            highlightthickness=0
+            highlightthickness=0,
+            show="*"
         )
-        entry_2.place(
-            x=688.0,
-            y=380.0,
-            width=259.0,
-            height=33.0
-        )
+        self.entry_2.place(x=688.0, y=380.0, width=259.0, height=33.0)
 
-        entry_3 = Entry(
+        self.entry_3 = Entry(
+            self.canvas,
             bd=0,
             bg="#FFFFFF",
             fg="#000716",
-            highlightthickness=0
+            highlightthickness=0,
+            font=("Arial Rounded MT Bold", 10)
         )
-        entry_3.place(
-            x=688.0,
-            y=287.0,
-            width=259.0,
-            height=38.0
-        )
+        self.entry_3.place(x=688.0, y=287.0, width=259.0, height=38.0)
 
-        # Place the canvas on the frame
-        canvas.pack(fill="both", expand=True)
+        self.canvas.pack(fill="both", expand=True)
 
-        parent.window.mainloop()
