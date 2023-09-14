8efaaae2a84126c91b63b25e83c69545a0935b0e
Zilu Wang <zwang3208@gatech.edu>
2023-09-14 00:51:45 -0400

    discord js version

--- discord_bot/bot_js/src/index.js
+++ discord_bot/bot_js/src/index.js
@@ -1,1 +1,29 @@
+require('dotenv').config();
 const {Client, IntentsBitField} = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds, 
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log('Bot is ready');
+    console.log(`{c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'ping') {
+        message.channel.send('pong');
+    }
+});
+
+
+client.login(process.env.TOKEN);
